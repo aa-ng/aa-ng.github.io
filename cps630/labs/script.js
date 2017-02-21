02314de9c05f92687fb7ff66a9a0338e7e29be5d
Cynicalbird <alex.ng.personal@gmail.com>
2017-02-20 23:02:08 -0500

    created cps630 labs page and added new photo to rsc/img

---
+++ cps630/labs/script.js
@@ -0,0 +1,25 @@
+
+window.onscroll = function (e)
+{
+	var brand = document.getElementById("brand");
+	var header = document.getElementById("header");
+	var slogan = document.getElementById("slogan");
+	var body = document.getElementsByTagName("body")[0];
+
+	var offset = parseInt(window.getComputedStyle(body, null).getPropertyValue('padding-top').substring(0,3));
+
+	if (window.scrollY >= offset)
+	{
+		//header.style.padding = "5px";
+		header.classList.add("expand");
+		brand.classList.add("expand");
+		slogan.style.display = "none";
+	}
+	else
+	{
+		//header.style.padding = "28px";
+		header.classList.remove("expand");
+		brand.classList.remove("expand");
+		slogan.style.display = "initial";
+	}
+};
