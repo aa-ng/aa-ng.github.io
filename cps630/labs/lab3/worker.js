2120621d509cd6565838433dba76452a0d41439d
Cynicalbird <alex.ng.personal@gmail.com>
2017-03-05 22:05:08 -0500

    testing

--- cps630/labs/lab3/worker.js
+++ cps630/labs/lab3/worker.js
@@ -1,12 +1,28 @@
+var coords;
+
 self.onmessage = function(message)
 {
-	postMessage(""+message[0]);
+	coords = message;
+	console.log(message.length);
+	console.log(message);
+	//postMessage(""+message[0]);
 }
 
 function haversine() {
     
+
+    var message; 
+    for (var i = 0; i < coords.length; i++)
+    {
+    	message = message+toRad(lat2-lat1);
+    } 
     //postMessage("");
     //setTimeout("timedCount()",500);
+}
+
+function toRad(x) 
+{
+   return x * Math.PI / 180;
 }
 
 haversine();
