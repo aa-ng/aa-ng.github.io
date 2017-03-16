966579ab54870f26a0f61b21e714247c58366e51
alex ng <alexng@aang.local>
2017-03-16 10:34:09 -0400

    testing angular controller

--- cps630/labs/lab4/script.js
+++ cps630/labs/lab4/script.js
@@ -1,12 +1,13 @@
-var todo_url = 'todo.json'
 
 //var todo = angular.module('todo', []);
 //todo.controller('todoController', function($scope) {
+<!--
 function todoController ($scope)
 {
 	$scope.list = requestTodo().todo;
 	log('todoController',$scope.list);
 }
+-->
 //});
 
 function addTodo()
