9edddb7a8cf19472075ec709356e49a895215121
Cynicalbird <alex.ng.personal@gmail.com>
2017-04-07 20:15:37 -0400

    Added current year to bottom of page

--- cps630/labs/lab5/routes/about.js
+++ cps630/labs/lab5/routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Handle '/' of about */
 router.get('/', function (req, res, next) {
-	res.render('about', {title:' About me', path: '/about'});
+	res.render('about', {title:' About me', path: '/about', year: new Date().getFullYear()});
 });
 
 module.exports = router;
--- cps630/labs/lab5/routes/index.js
+++ cps630/labs/lab5/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', path: req.path});
+  res.render('index', { title: 'Home', path: req.path, year: new Date().getFullYear()});
 });
 
 module.exports = router;
